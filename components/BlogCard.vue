5268f1ccd19c602be9be9913786a0189e94aaf86
Ha Le <nhiha001@gmail.com>
2021-09-23 02:34:36 +0200

    Fix align

--- components/BlogCard.vue
+++ components/BlogCard.vue
@@ -1,7 +1,7 @@
 <template>
   <c-box v-bind:class="{ active: active}" @click="activeCard" :cardNumber="cardNumber" :fontSize="['1.5rem','1.5rem', '2rem']"
     :p="['20px', '20px', '50px 50px 35px']">
-    <div class="card-content text-center">
+    <c-box class="card-content" textAlign="center">
       <span class="gray">Blog</span>
       <span class="pink">ML & Web Application</span>
       <c-text fontSize="sm" mt="3" mb="3">
@@ -11,7 +11,7 @@
       </c-text>
       <c-button class="bg-red-400" rounded="20px" v-scroll-to="{
      el: '#blog-content', offset: -200, lazy: false}">Start Reading</c-button>
-    </div>
+    </c-box>
   </c-box>
 </template>
 <script>
